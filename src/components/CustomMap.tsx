44a10ff1672f1a384a47063f09861ca3467ee7b2
Sagar Chand <chandsagar314@gmail.com>
2024-08-23 13:52:14 +0545

    created marker component

--- src/components/CustomMap.tsx
+++ src/components/CustomMap.tsx
@@ -1,6 +1,7 @@
-import OLVectorTileLayer from "@/components/map/layers/OLVectorTileLayer";
+import OLVectorTileLayer from "@/components/map/OLVectorTileLayer";
 import OLMap from "@/components/map/OLMap";
 import "ol/ol.css";
+import Marker from "./map/Marker";
 
 // ? https://vectortile.naxa.com.np/federal/${vectorLayer}.mvt/?tile={z}/{x}/{y}
 
@@ -8,10 +9,13 @@ const CustomMap = () => {
   return (
     <OLMap zoom={6} center={[84.124, 28.3949]}>
       {(map) => (
-        <OLVectorTileLayer
-          map={map}
-          url="https://vectortile.naxa.com.np/federal/district.mvt/?tile={z}/{x}/{y}"
-        />
+        <>
+          <OLVectorTileLayer
+            map={map}
+            url="https://vectortile.naxa.com.np/federal/district.mvt/?tile={z}/{x}/{y}"
+          />
+          <Marker map={map} />
+        </>
       )}
     </OLMap>
   );
---
+++ src/components/map/Marker.tsx
@@ -0,0 +1,59 @@
+import Feature from "ol/Feature";
+import Map from "ol/Map";
+import Point from "ol/geom/Point";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { Icon, Style } from "ol/style";
+import { useEffect, useState } from "react";
+
+const Marker = ({ map }: { map: Map }) => {
+  const [markerLayer, setMarkerLayer] = useState<VectorLayer | null>(null);
+
+  useEffect(() => {
+    if (map) {
+      const vectorSource = new VectorSource();
+
+      const layer = new VectorLayer({
+        source: vectorSource,
+      });
+
+      setMarkerLayer(layer);
+      map.addLayer(layer);
+
+      const handleMapClick = (event: any) => {
+        const coordinates = map.getCoordinateFromPixel(event.pixel);
+
+        // clear previous marker
+        vectorSource.clear();
+
+        const marker = new Feature({
+          geometry: new Point(coordinates),
+        });
+
+        const markerStyle = new Style({
+          image: new Icon({
+            src: "https://png.pngtree.com/png-vector/20210214/ourmid/pngtree-location-marker-png-image_2921053.jpg",
+            anchor: [0.5, 1],
+            scale: 0.06,
+          }),
+        });
+
+        marker.setStyle(markerStyle);
+        vectorSource.addFeature(marker);
+      };
+
+      map.on("click", handleMapClick);
+
+      return () => {
+        map.un("click", handleMapClick);
+        if (markerLayer) {
+          map.removeLayer(markerLayer);
+        }
+      };
+    }
+  }, [map]);
+
+  return null;
+};
+
+export default Marker;
